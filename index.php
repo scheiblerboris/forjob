9047ace91c8cdce3e1dd30ff8740c3eeb9be642f
Bor333 <mugen999999999@gmail.com>
2024-04-27 06:36:39 +0700

    READ

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 $db = mysqli_connect('localhost', 'forjob', 'forjob', 'forjob');
 
 $query = mysqli_query($db, "SELECT * FROM `employee` WHERE 1");
-
-var_dump($query);
+?>
+<h1>Список сотрудников</h1>
+<?php foreach ($query as $item): ?>
+        <p>Имя: <?= $item['name'] ?></p>
+        <p>Фамилия: <?= $item['surname'] ?></p>
+        <p>Должность: <?= $item['position'] ?></p>
+        <p>Зарплата: <?= $item['salary'] ?></p>
+    <br>
+<?php endforeach; ?>
