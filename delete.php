7f26a86c2825cfdeec92a1112d1fabdc21141fd7
Bor333 <mugen999999999@gmail.com>
2024-04-27 07:13:07 +0700

    test delete

--- delete.php
+++ delete.php
@@ -1,2 +1,7 @@
+<?php
+include './db.php';
+$query = mysqli_query($db, "DELETE FROM `employee` WHERE id = 1");
+echo 'deleted';
+?>
 <?php include './menu.php' ?>
-Delete
+<!--<h1>Уволить сотрудника</h1>-->
