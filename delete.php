e3ecb97ef80967f7095efb010dbbabaf901f67e3
Bor333 <mugen999999999@gmail.com>
2024-04-27 08:37:15 +0700

    delete done

--- delete.php
+++ delete.php
@@ -1,7 +1,28 @@
 <?php
 include './db.php';
-$query = mysqli_query($db, "DELETE FROM `employee` WHERE id = 1");
-echo 'deleted';
+$query = mysqli_query($db, "SELECT * FROM `employee` WHERE 1");
+if (!empty($_POST)) {
+    $sql = "DELETE FROM `employee` WHERE id = {$_POST['id']}";
+    $query = mysqli_query($db, $sql);
+    header("Location: ?");
+    die();
+}
+
+
+
 ?>
 <?php include './menu.php' ?>
-<!--<h1>Уволить сотрудника</h1>-->
+
+<form action="delete.php" method="post" enctype="multipart/form-data">
+    <h1>Список сотрудников</h1>
+    <?php foreach ($query as $item): ?>
+        <input type="text" hidden name="id" value="<?= $item['id'] ?>">
+        <p>Имя: <?= $item['name'] ?></p>
+        <p>Фамилия: <?= $item['surname'] ?></p>
+        <p>Должность: <?= $item['position'] ?></p>
+        <p>Зарплата: <?= $item['salary'] ?></p>
+        <br>
+<!--        <input type="submit" value="уволить">-->
+    <button>Уволить</button>
+    <?php endforeach; ?>
+</form>
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 include './db.php';
 $query = mysqli_query($db, "SELECT * FROM `employee` WHERE 1");
+
+function deleteFromDb($db, $id) {
+    $query = mysqli_query($db, "DELETE FROM `employee` WHERE id = {$id}");
+}
+
 ?>
 <?php include './menu.php' ?>
 <h1>Список сотрудников</h1>
